d2bf16ec481d8a39bd0ce479016bc7107ce95fb8
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2023-05-28 11:42:32 -0700

    fleshed out helper methods

--- src/lexer.v
+++ src/lexer.v
@@ -9,28 +9,28 @@ pub mut:
 	literal T
 }
 
-pub fn new_token[T](id string, lit T) {}
+// new_token returns a token with the appropriate type.
+pub fn new_token[T](id string, lit T) GenericToken[T] {
+	return GenericToken[T]{
+		literal: lit
+		id: id
+	}
+}
 
-// NOTE: This is the old way, without generics
-// pub fn new_token(ttype TokenType, ch byte) token.Token {
-// 	return token.Token
-// 	{
-// 		literal:
-// 		ch.ascii_str()
-// 		token_type:
-// 		ttype
-// 	}
-// }
-
+// we'll keep this around just to make debugging things a bit easier.
+// I can't think of any other use for it
 pub fn (toki GenericToken[T]) str() string {
 	return 'Type: ${toki.id} Value: ${toki.literal}'
 }
 
+// this is a random comment
 pub struct Lexer {
 	// this is reserved for future work
 	regard_whitespace bool
+	// standard types, custom ones can be added
+	types map[string]string
 mut:
-	// this is too
+	// these two are  also reserved for future work
 	innumber bool
 	instring bool
 pub:
@@ -44,14 +44,23 @@ pub mut:
 	identifiers   TokenMap
 }
 
+// new returns a Lexer instance, once given input, and a token and keyword map
 pub fn new(inp string, tkns TokenMap, kwds TokenMap) Lexer {
 	mut retv := Lexer{
 		input: inp
 		tokens: tkns
 		keywords: kwds
 	}
+	// initialize the struct and get it ready for parsing
 	retv.read_char()
 	return retv
+}
+
+pub fn (mut lexx Lexer) next_token[T]() GenericToken[T] {
+	mut tok := token.GenericToken[T]
+	{
+	}
+	return tok
 }
 
 pub fn (mut lexx Lexer) read_char() {
@@ -64,7 +73,7 @@ pub fn (mut lexx Lexer) read_char() {
 	lexx.read_position += 1
 }
 
-pub fn (mut lexx Lexer) peek_char() byte {
+fn (mut lexx Lexer) peek_char() byte {
 	if lexx.read_position >= lexx.input.len {
 		return 0
 	} else {
@@ -72,7 +81,7 @@ pub fn (mut lexx Lexer) peek_char() byte {
 	}
 }
 
-pub fn (mut lexx Lexer) ignore_whitespace() {
+fn (mut lexx Lexer) ignore_whitespace() {
 	if !lexx.regard_whitespace {
 		for lexx.current == ` ` || lexx.current == `\t` || lexx.current == `\n`
 			|| lexx.current == `\r` {
@@ -81,19 +90,33 @@ pub fn (mut lexx Lexer) ignore_whitespace() {
 	}
 }
 
-// TODO: This is s stub
-pub fn (mut lexx Lexer) read_identifier() string {
-	return ' '
+// identifiers are alpha only + _ so read till there's no more of that
+fn (mut lexx Lexer) read_identifier() string {
+	pos := lexx.position
+	for is_letter(lexx.current) {
+		lexx.read_char()
+	}
+	return lexx.input[pos..lexx.position]
 }
 
-// TODO: This is a stub
-pub fn (mut lexx Lexer) read_string() string {
-	return ' '
+// strings go from opening char to closing char, which match, so this is easy
+fn (mut lexx Lexer) read_string() string {
+	pos := lexx.position
+	lexx.read_char()
+	for lexx.current != lexx.input[pos] {
+		lexx.read_char()
+	}
+	end := lexx.position + 1
+	return lexx.input[pos..end]
 }
 
-// TODO: this is a stub
-pub fn (mut lexx Lexer) read_number() string {
-	return ' '
+// numbers can include a period, assuming there is a number after it.
+fn (mut lexx Lexer) read_number() string {
+	pos := lexx.position
+	for is_digit(lexx.current) {
+		lexx.read_char()
+	}
+	return lexx.input[pos..lexx.position]
 }
 
 // lookup an identifier to see if it is valid or not.
@@ -113,8 +136,6 @@ pub fn (mut lexx Lexer) lookup_keyword(id string) TokenType {
 	retv := lexx.keywords[id] or { return 'IDENT' }
 	return retv
 }
-
-pub fn (mut lexx Lexer) next_token[T]() GenericToken[T] {}
 
 fn is_letter(ch byte) bool {
 	return (`a` <= ch && ch <= `z`) || (`A` <= ch && ch <= `Z`) || ch == `_`
